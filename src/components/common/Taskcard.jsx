9c67939ae9d61f9092032b8543b532897fe47d0c
AravindDevaraj-24 <aravind.devaraj.024@gmail.com>
2026-03-28 20:24:33 +0530

    created TaskCard component

---
+++ src/components/common/Taskcard.jsx
@@ -0,0 +1,43 @@
+import { Box, Heading, Text, Badge, VStack, HStack } from "@chakra-ui/react";
+
+const Taskcard = ({ task })=> {
+
+  const priorityColor = (priority) => {
+    if(priority === "high") return "red";
+    if(priority === "medium") return "yellow";
+    return "green"
+  }
+
+  const statusColor = (status) => {
+    if(status === "todo") return "gray";
+    if(status === "in-progress") return "blue";
+    return "green"
+  }
+  return (
+    <Box
+      p="5"
+      borderWidth="1"
+      borderRadius="lg"
+      boxShadow="sm"
+      width="100%"
+    >
+      <VStack align="start" spacing="3">
+
+        <Heading>{task.title}</Heading>
+
+        <Text>{task.description}</Text>
+
+        <HStack spacing="3">
+
+          <Badge colorScheme={priorityColor(task.priority)} >{task.priority}</Badge>
+
+          <Badge colorScheme={statusColor(task.status)}>{task.status}</Badge>
+          
+        </HStack>
+
+      </VStack>
+    </Box>
+  )
+}
+
+export default Taskcard
--- src/pages/Tasks/Tasks.jsx
+++ src/pages/Tasks/Tasks.jsx
@@ -1,22 +1,23 @@
-import { useContext } from "react";
-import { TaskContext } from "../../context/TaskContext";
-import { Heading, Text } from "@chakra-ui/react";
-
+import { useContext } from 'react';
+import { TaskContext } from '../../context/TaskContext';
+import { Box, Heading, Text, VStack } from '@chakra-ui/react';
+import Taskcard from '../../components/common/Taskcard';
 
 function Tasks() {
-
-  const {tasks} = useContext(TaskContext)
+  const { tasks } = useContext(TaskContext);
   return (
-    <div>
-    <Heading mb="6">Tasks Page</Heading>
-
-    {tasks.map((task, index)=>(
-      <div key={index}>
-        <Heading size={"lg"}>{task.title}</Heading>
-        <Text fontSize="2xl">{task.description}</Text>
-      </div>
-    ))}
-     </div>
+    <Box>
+      <Heading mb='6'>Tasks Page</Heading>
+      {tasks.length === 0 ? (
+        <Text>No Tasks Created Yet</Text>
+      ) : (
+        <VStack spacing='4' align='stretch'>
+          {tasks.map((task, index) => (
+            <Taskcard key={index} task={task} />
+          ))}
+        </VStack>
+      )}
+    </Box>
   );
 }
 
