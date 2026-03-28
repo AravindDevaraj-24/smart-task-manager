0749d6b1d8579e6d915a7d74d72f782c673a9132
AravindDevaraj-24 <aravind.devaraj.024@gmail.com>
2026-03-28 19:35:49 +0530

    Added local storage for Previous Data

--- src/context/TaskContext.jsx
+++ src/context/TaskContext.jsx
@@ -1,14 +1,21 @@
-import { createContext, useState } from "react";
+import { createContext, useEffect, useState } from "react";
 
 export const TaskContext = createContext();
 
 export const TaskProvider = ({children}) => {
 
-  const [tasks, setTasks] = useState([]);
-
+  const [tasks, setTasks] = useState(()=>{
+    const storedData = localStorage.getItem("tasks");
+    return storedData ? JSON.parse(storedData) : []
+  });
+  
   const addTask = (task)=> {
     setTasks((prev)=>[...prev, task]);
   }
+
+  useEffect(()=>{
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks])
 
   return (
     <TaskContext.Provider value={{ tasks, addTask }}>
